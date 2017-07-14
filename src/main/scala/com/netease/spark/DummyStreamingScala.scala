41412a8efa5ad35eeec32a15c96ff6639c30c3e2
lishuming <alemmontree@126.com>
2017-07-14 15:39:28 +0800

    add updateStateByKey usage

--- src/main/scala/com/netease/spark/DummyStreamingScala.scala
+++ src/main/scala/com/netease/spark/DummyStreamingScala.scala
@@ -1,13 +1,13 @@
 package com.netease.spark
 
 import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.spark.{SparkConf}
+import org.apache.spark.{HashPartitioner, SparkConf}
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.kafka010._
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 
-object DymmyStreamingScala {
+object DummyStreamingScala {
 
   def main(args: Array[String]): Unit = {
 
@@ -22,7 +22,6 @@ object DymmyStreamingScala {
 
 
     val conf = new SparkConf().setAppName("Kafka10 Streaming")
-    conf.set("spark.yarn.access.namenodes", "hdfs://hz-cluster3,hdfs://hz-cluster4")
     val ssc = new StreamingContext(conf, Seconds(2))
 
     val topics = Array("spark-logs")
@@ -31,11 +30,13 @@ object DymmyStreamingScala {
       PreferConsistent,
       Subscribe[String, String](topics, kafkaParams))
 
+    ssc.checkpoint("/tmp/spark-checkpoint")
 
     /**
      val wordCount = stream.map(_.value()).flatMap(_.split(" "))
       .filter(_.equalsIgnoreCase("exception")).map(x => (x, 1L))
       .reduceByKeyAndWindow(_ + _, _ - _, Minutes(2), Seconds(30), 2)*/
+    //wordCount.print()
 
     // TOP N Problem
     /**
@@ -47,16 +48,40 @@ object DymmyStreamingScala {
           .saveAsTextFile("/tmp/spark-streaming-demo-1/wordCount")
       })*/
 
+    /**
     val  topN = stream.map(_.value()).flatMap(_.split(" "))
-      .map(x => (x, 1L)).reduceByKey(_ + _).map(x => (x._2, x._1))
+      .map(x => (x, 1L)).window(Minutes(2), Seconds(30))
+      .reduceByKey(_ + _)
+      .map(x => (x._2, x._1))
       .transform(rdd =>
-        rdd.sortByKey(false).map(x => (x._2, x._1)))
-    topN.saveAsTextFiles("/tmp/spark-streaming-demo-2/wordCount")
+        rdd.sortByKey(false).map(x => (x._2, x._1))
+      )
     //topN.print()
+    topN.saveAsTextFiles("/tmp/spark-streaming-demo-2/wordCount")*/
 
-    //wordCount.print()
-    //wordCount.saveAsTextFiles("/tmp/spark-streaming-demo/wordCount")
-    ssc.checkpoint("/tmp/spark-checkpoint")
+    // State
+    val updateFunc = (v: Seq[Long], s: Option[Long]) => {
+      val current = v.sum
+      val previous = s.getOrElse(0L)
+
+      Some(current + previous)
+    }
+
+    val newUpdateFunc = (iter: Iterator[(String, Seq[Long], Option[Long])]) => {
+      iter.flatMap(t => updateFunc(t._2, t._3).map(s => (t._1, s)))
+    }
+
+    val sRDD = stream.map(_.value()).flatMap(_.split(" "))
+      .map(x => (x, 1L))
+      .updateStateByKey[Long](newUpdateFunc, new HashPartitioner(2), true)
+    sRDD.foreachRDD(rdd => {
+      val sortRDD = rdd.map{case (k, v) => (v, k)}.sortByKey(false)
+      val topNData = sortRDD.take(10).map{case (v, k) => (k, v)}
+      topNData.foreach( x =>
+        println("!!!!Result:" + x)
+      )
+    })
+    //sRDD.saveAsTextFiles("/tmp/spark-streaming-demo-2/wordCount")
 
     /**stream.foreachRDD(rdd =>
     rdd.foreachPartition { iter =>
