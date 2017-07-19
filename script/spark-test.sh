64361e392284efd5ea2f3322967bcf194968536e
hzlishuming <hzlishuming@corp.netease.com>
2017-07-19 14:51:49 +0800

    update script

--- script/spark-test.sh
+++ script/spark-test.sh
@@ -1,3 +1,3 @@
 kinit -kt ./spark-demo-202/mammut.keytab mammut/bigdata@HADOOP.HZ.NETEASE.COM
 export SPARK_HOME=/home/hadoop/spark-2.0.2-bin-hadoop2.7
-/home/hadoop/spark-2.0.2-bin-hadoop2.7/bin/spark-submit --class com.netease.spark.DummyCountJava --master yarn  --deploy-mode client --driver-memory 2048M --executor-memory 512M --executor-cores 1 --queue default ./spark-demo-202/spark-demo-202.jar
+/home/hadoop/spark-2.0.2-bin-hadoop2.7/bin/spark-submit --conf spark.yarn.keytab=./spark-demo-202/mammut.keytab  --conf spark.yarn.principal=mammut/bigdata@HADOOP.HZ.NETEASE.COM --class com.netease.spark.DummyCountJava --master yarn  --deploy-mode client --driver-memory 2048M --executor-memory 512M --executor-cores 1 --queue default ./spark-demo-202/spark-demo-202.jar
