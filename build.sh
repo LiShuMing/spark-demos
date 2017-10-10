6dc9bca64b314a8812cd0664e1a1dd9485955a18
lishuming <alemmontree@126.com>
2017-10-10 17:53:38 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -21,13 +21,13 @@ mkdir -p ${SPARK_DEMO}
 cp ./target/spark-demo-0.1.0-jar-with-dependencies.jar ./${SPARK_DEMO}/spark-demo-${SPARK_VERSION}.jar
 cp ./conf/project.properties ./${SPARK_DEMO}
 
-#cp ./conf/spark-demo.flow ./${SPARK_DEMO}
-#sed s/spark-demo/spark-demo-${SPARK_VERSION}/g ${SPARK_DEMO}/spark-demo.flow > spark-demo.flow.tmp
-#mv spark-demo.flow.tmp ./${SPARK_DEMO}/spark-demo.flow
-#
-#cp ./conf/spark-demo.job ./${SPARK_DEMO}
-#sed s/spark-demo/spark-demo-${SPARK_VERSION}/g ${SPARK_DEMO}/spark-demo.job|sed s/spark-version=2.0.2/spark-version=${SPARK_REAL_VERSION}/g > spark-demo.job.tmp
-#mv spark-demo.job.tmp ./${SPARK_DEMO}/spark-demo.job
+cp ./conf/spark-demo.flow ./${SPARK_DEMO}
+sed s/spark-demo/spark-demo-${SPARK_VERSION}/g ${SPARK_DEMO}/spark-demo.flow > spark-demo.flow.tmp
+mv spark-demo.flow.tmp ./${SPARK_DEMO}/spark-demo.flow
+
+cp ./conf/spark-demo.job ./${SPARK_DEMO}
+sed s/spark-demo/spark-demo-${SPARK_VERSION}/g ${SPARK_DEMO}/spark-demo.job|sed s/spark-version=2.0.2/spark-version=${SPARK_REAL_VERSION}/g > spark-demo.job.tmp
+mv spark-demo.job.tmp ./${SPARK_DEMO}/spark-demo.job
 
 
 zip -r ${SPARK_DEMO}.zip ./${SPARK_DEMO}
